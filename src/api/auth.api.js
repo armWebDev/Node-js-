71f6449b007b23dc7b693ca05485840ab7c49b02
armWebDev <tadevosyana075@gmail.com>
2024-02-15 19:33:24 +0400

    15.02.2024

---
+++ src/api/auth.api.js
@@ -0,0 +1,17 @@
+// NPM Modules
+import express from 'express';
+
+import AuthController from '../auth/auth.controller';
+import { AuthValidationMiddleware } from '../middlewares/validation';
+
+const router = express.Router();
+
+// Մուտք
+
+router.post('/login',
+  AuthValidationMiddleware.validateLoginArgs,
+  AuthController.login);
+
+router.post('/refresh', AuthController.refresh);
+
+export default router;
